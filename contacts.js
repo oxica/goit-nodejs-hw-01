b8ec7b66f750f5b2b9ccda5ae36619867da14bcc
Oksana <kskaplun@gmail.com>
2022-07-05 23:17:32 +0300

    add list

---
+++ contacts.js
@@ -0,0 +1,28 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.resolve(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+  try {
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {}
+
+async function removeContact(contactId) {}
+
+async function addContact(name, email, phone) {}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+const contactsOperations = require("./contacts.js");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      // ... id
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
